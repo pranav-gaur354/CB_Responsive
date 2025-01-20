9a367772bd9d6a239d62c395954cb7ed382f0f5e
Aditya Rawat <adityarawat240@gmail.com>
2025-01-21 00:58:37 +0530

    Basic ui fixes on home and about page, raleway font added

--- src/modules/home/Home.js
+++ src/modules/home/Home.js
@@ -6,19 +6,19 @@ import Typewriter from "typewriter-effect";
 export const Home = () => {
   return (
     <>
-      <div className="w-screen" id="home">
+      <div className="w-screen h-screen max-h-screen flex flex-col items-center justify-center bg-black border-b-2 border-white" id="home">
         <div className="home_outer">
           <Background_animation />
           <div className="set home_inner">
             <div className="home_inner_logo">
               <img src={Logo} alt="logo..."></img>
             </div>
-            <div className="home_inner_content font_society" >
+            <div className="text-white text-xl w-[235px] sm:text-3xl sm:w-[350px] md:text-5xl italic font-semibold md:w-[570px]" >
               CAMPUS BLOCKS BVCOE
             </div>
-            <div className="home_inner_typewriter font">
-              <span className="home_inner_typewriter_gap">WHERE BYTES CREATE </span>
-              <span>
+            <div className="text-white flex w-72">
+              <p className="pr-1">WHERE BYTES CREATE</p>
+              <p>
                 <Typewriter
                   options={{
                     strings: ["SUCCESS", "PASSION","BONDS"],
@@ -26,7 +26,7 @@ export const Home = () => {
                     loop: true,
                   }}
                 />
-              </span>
+              </p>
             </div>
           </div>
         </div>
